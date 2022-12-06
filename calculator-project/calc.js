895f95bf8a61cf36cfd8ca69764d20f25c31ad09
nebulamarie <mepps1115@gmail.com>
2022-12-05 21:19:47 -0500

    mess

---
+++ calculator-project/calc.js
@@ -0,0 +1,30 @@
+const numberButtons = document.querySelectorAll('data-number') 
+const operationButtons = document.querySelectorAll('data-operation') 
+const equalsButton = documnet.querySelector('data-equals') 
+const deleteButton = documnet.querySelector('data-delete') 
+const clearButton = documnet.querySelector('data-all-clear') 
+const plusminusButton = document.querySelector('data-plus-or-minus') 
+
+ 
+ 
+ 
+
+let number = documnet.querySelectorAll('data-number') 
+let operation =document.querySelectorAll('data-operation') 
+let equal = document.querySelector('data-equals') 
+let del = document.querySelector('data-delete') 
+let clear = document.querySelector('data-all-clear') 
+let plusMinus =docucument.querySelector('data-plus-or-minus') 
+
+ 
+ 
+
+button.addEventListener('click', e=>{ 
+
+ 
+ 
+
+}) 
+
+ 
+
